2eb5fec8587b3700d4b7d6cfdfc06e095803b459
PANGHWAN2 <raccoonste@naver.com>
2024-06-26 16:37:37 +0900

    update

---
+++ boj/boj_1260.java
@@ -0,0 +1,81 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class boj_1260 {
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] check;
+    static int[][] arr;
+
+    static int node, line, start;
+
+    static Queue<Integer> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        node = Integer.parseInt(st.nextToken());
+        line = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+
+        arr = new int[node + 1][node + 1];
+        check = new boolean[node + 1];
+
+        for (int i = 0; i < line; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        dfs(start);
+        sb.append("\n");
+        check = new boolean[node + 1];
+
+        bfs(start);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int start) {
+        check[start] = true;
+        sb.append(start + " ");
+
+        for (int i = 0; i <= node; i++) {
+            if (arr[start][i] == 1 && !check[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        q.add(start);
+        check[start] = true;
+
+        while (!q.isEmpty()) {
+            start = q.poll();
+            sb.append(start + " ");
+
+            for (int i = 1; i <= node; i++) {
+                if (arr[start][i] == 1 && !check[i]) {
+                    q.add(i);
+                    check[i] = true;
+                }
+            }
+        }
+    }
+}
---
+++ boj/boj_25501.java
@@ -0,0 +1,40 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class boj_25501 {
+    public static int cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            cnt = 0;
+            int result = isPalindrome(s);
+            bw.write(result + " " + cnt + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static int recursion(String s, int l, int r) {
+        cnt++;
+        if (l >= r)
+            return 1;
+        else if (s.charAt(l) != s.charAt(r))
+            return 0;
+        else
+            return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+}
